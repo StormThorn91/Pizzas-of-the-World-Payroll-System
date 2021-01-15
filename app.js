c5eb727f4c71adfa2d84e7a0225d44e7db938e97
StormThorn91 <caitlinsistoso@gmail.com>
2021-01-15 23:02:22 +0800

    initial login and admin page

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var adminName = 'Caitlin';
+
+app.set('view engine', "ejs");
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+    res.render('home');
+});
+
+app.get('/admin/:name', (req, res) => {
+    res.render('admin', {adminName: req.params.name});
 });
 
 app.listen(port,() => {
