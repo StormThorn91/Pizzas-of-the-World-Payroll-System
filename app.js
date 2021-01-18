05add206a20586b53cf712ed74f91341d5e548bc
Caitlin Sistoso <caitvsistoso@gmail.com>
2021-01-19 03:17:58 +0800

    With test sign up page

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ var adminName = 'Caitlin';
 
 app.set('view engine', "ejs");
 
+//for ejs files to be able to apply the css filess
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
+    console.log(req.query);
     res.render('home');
+});
+
+app.get('/sign-up', (req, res) => {
+    console.log(req.query);
+    res.render('sign-up', {qs: req.query});
 });
 
 app.get('/admin/:name', (req, res) => {
